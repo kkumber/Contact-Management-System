13f84ac1b8ba01b00d1dbb8978716187bfbbe555
Angel Bert Reyes <areyes@insightqgroup.com>
2026-08-19 20:56:51 +0800

    feat: json config

---
+++ JsonConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ContactManagementSystem
+{
+    static class JsonConfig
+    {
+        public static readonly JsonSerializerOptions Options = new({ WriteIndented = true })
+    }
+
+}
--- JsonHandler.cs
+++ JsonHandler.cs
@@ -5,6 +5,7 @@ namespace ContactManagementSystem
     class JsonHandler
     {
         public ContactManagement Manager { get; }
+        public bool WriteIndentedOption = true;
 
         public JsonHandler(ContactManagement contacts) 
         { 
@@ -12,7 +13,15 @@ namespace ContactManagementSystem
         }
 
         // save 1 contact to json on create
+        public void SaveContactToJson(Person contact)
+        {
+           
+        }   
+
+
         // save all contacts in manager to json on exit
+        // save an edited contact or deleted
         // initialize manager with contacts from json on start
+
     }
 }
